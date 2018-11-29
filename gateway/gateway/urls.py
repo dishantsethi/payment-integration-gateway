b5ba0c45b18ee7c0ae4b4f01c094c8b0e87f2ed7
dishantsethi <dishantsethi14@gmail.com>
2018-11-30 00:27:14 +0530

    initial stage

--- gateway/gateway/urls.py
+++ gateway/gateway/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,urls
+from . import views
 
 urlpatterns = [
+    urls('payment/',views.pay_button),
     path('admin/', admin.site.urls),
 ]
